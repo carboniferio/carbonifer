24269196fdfef5f3746059a421f897409cbe050c
Olivier Bierlaire <olivier@carbonifer.io>
2023-09-08 14:40:42 +0200

    Missing Tests after reorg (#86)

--- internal/plan/test/resources_gcp_test.go
+++ internal/plan/test/resources_gcp_test.go
@@ -1,15 +1,19 @@
 package plan_test
 
 import (
+	"log"
+	"path"
 	"testing"
 
 	"github.com/carboniferio/carbonifer/internal/plan"
 	"github.com/carboniferio/carbonifer/internal/providers"
 	"github.com/carboniferio/carbonifer/internal/resources"
+	"github.com/carboniferio/carbonifer/internal/terraform"
 	"github.com/carboniferio/carbonifer/internal/testutils"
 	_ "github.com/carboniferio/carbonifer/internal/testutils"
 	tfjson "github.com/hashicorp/terraform-json"
 	"github.com/shopspring/decimal"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -228,4 +232,169 @@ func TestGetResource(t *testing.T) {
 			assert.NoError(t, err)
 		})
 	}
+}
+
+func TestGetResources_DiskImage(t *testing.T) {
+	testutils.SkipWithCreds(t)
+	// reset
+	terraform.ResetTerraformExec()
+
+	t.Setenv("GOOGLE_OAUTH_ACCESS_TOKEN", "")
+
+	wd := path.Join(testutils.RootDir, "test/terraform/gcp_images")
+	viper.Set("workdir", wd)
+
+	wantResources := map[string]resources.Resource{
+		"google_compute_disk.diskImage": resources.ComputeResource{
+			Identification: &resources.ResourceIdentification{
+				Address:      "google_compute_disk.diskImage",
+				Name:         "diskImage",
+				ResourceType: "google_compute_disk",
+				Provider:     providers.GCP,
+				Region:       "europe-west9",
+				Count:        1,
+			},
+			Specs: &resources.ComputeResourceSpecs{
+				GpuTypes:          nil,
+				HddStorage:        decimal.New(int64(10), 0),
+				SsdStorage:        decimal.Zero,
+				MemoryMb:          0,
+				VCPUs:             0,
+				CPUType:           "",
+				ReplicationFactor: 1,
+			},
+		},
+	}
+
+	tfPlan, _ := terraform.TerraformPlan()
+	resourceList, err := plan.GetResources(tfPlan)
+	if assert.NoError(t, err) {
+		assert.Equal(t, len(wantResources), len(resourceList))
+		for i, resource := range resourceList {
+			wantResource := wantResources[i]
+			log.Println(resource.(resources.ComputeResource).Specs.HddStorage)
+			assert.EqualValues(t, wantResource, resource)
+		}
+	}
+
+}
+
+func TestGetResources_GroupInstance(t *testing.T) {
+	// reset
+	terraform.ResetTerraformExec()
+
+	wd := path.Join(testutils.RootDir, "test/terraform/gcp_group")
+	viper.Set("workdir", wd)
+
+	wantResources := map[string]resources.Resource{
+		"google_compute_network.vpc_network": resources.UnsupportedResource{
+			Identification: &resources.ResourceIdentification{
+				Address:      "google_compute_network.vpc_network",
+				Name:         "vpc_network",
+				ResourceType: "google_compute_network",
+				Provider:     providers.GCP,
+				Region:       "",
+				Count:        1,
+			},
+		},
+		"google_compute_subnetwork.first": resources.UnsupportedResource{
+			Identification: &resources.ResourceIdentification{
+				Address:      "google_compute_subnetwork.first",
+				Name:         "first",
+				ResourceType: "google_compute_subnetwork",
+				Provider:     providers.GCP,
+				Region:       "",
+				Count:        1,
+			},
+		},
+		"google_compute_instance_group_manager.my-group-manager": resources.ComputeResource{
+			Identification: &resources.ResourceIdentification{
+				Address:      "google_compute_instance_group_manager.my-group-manager",
+				Name:         "my-group-manager",
+				ResourceType: "google_compute_instance_group_manager",
+				Provider:     providers.GCP,
+				Region:       "europe-west9",
+				Count:        3,
+			},
+			Specs: &resources.ComputeResourceSpecs{
+				GpuTypes:          nil,
+				HddStorage:        decimal.New(20, 0),
+				SsdStorage:        decimal.Zero,
+				MemoryMb:          8192,
+				VCPUs:             2,
+				CPUType:           "",
+				ReplicationFactor: 1,
+			},
+		},
+	}
+
+	tfPlan, _ := terraform.TerraformPlan()
+	resources, err := plan.GetResources(tfPlan)
+	if assert.NoError(t, err) {
+		for i, resource := range resources {
+			wantResource := wantResources[i]
+			assert.EqualValues(t, wantResource, resource)
+		}
+	}
+
+}
+
+func TestGetResources_InstanceFromTemplate(t *testing.T) {
+	// reset
+	terraform.ResetTerraformExec()
+
+	wd := path.Join(testutils.RootDir, "test/terraform/gcp_cit")
+	viper.Set("workdir", wd)
+
+	wantResources := map[string]resources.Resource{
+		"google_compute_network.vpc_network": resources.UnsupportedResource{
+			Identification: &resources.ResourceIdentification{
+				Address:      "google_compute_network.vpc_network",
+				Name:         "vpc_network",
+				ResourceType: "google_compute_network",
+				Provider:     providers.GCP,
+				Region:       "",
+				Count:        1,
+			},
+		},
+		"google_compute_subnetwork.first": resources.UnsupportedResource{
+			Identification: &resources.ResourceIdentification{
+				Address:      "google_compute_subnetwork.first",
+				Name:         "first",
+				ResourceType: "google_compute_subnetwork",
+				Provider:     providers.GCP,
+				Region:       "",
+				Count:        1,
+			},
+		},
+		"google_compute_instance_from_template.ifromtpl": resources.ComputeResource{
+			Identification: &resources.ResourceIdentification{
+				Address:      "google_compute_instance_from_template.ifromtpl",
+				Name:         "ifromtpl",
+				ResourceType: "google_compute_instance_from_template",
+				Provider:     providers.GCP,
+				Region:       "europe-west9",
+				Count:        1,
+			},
+			Specs: &resources.ComputeResourceSpecs{
+				GpuTypes:          nil,
+				HddStorage:        decimal.New(20, 0),
+				SsdStorage:        decimal.Zero,
+				MemoryMb:          8192,
+				VCPUs:             2,
+				CPUType:           "",
+				ReplicationFactor: 1,
+			},
+		},
+	}
+
+	tfPlan, _ := terraform.TerraformPlan()
+	resources, err := plan.GetResources(tfPlan)
+	if assert.NoError(t, err) {
+		for i, resource := range resources {
+			wantResource := wantResources[i]
+			assert.EqualValues(t, wantResource, resource)
+		}
+	}
+
 }
